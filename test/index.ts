9a5cb75a87de92e1424d3cf6f0e8104cf6b0d3f2
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-01-13 18:36:03 +0530

    spectron testing on github actions for linux

--- test/index.ts
+++ test/index.ts
@@ -9,27 +9,32 @@ import {Application} from 'spectron';
 let app: Application;
 
 test.before(async () => {
-  let pathToBinary;
+  // let pathToBinary;
 
-  switch (process.platform) {
-    case 'linux':
-      pathToBinary = path.join(__dirname, '../dist/linux-unpacked/hyper');
-      break;
+  // switch (process.platform) {
+  //   case 'linux':
+  //     pathToBinary = path.join(__dirname, '../dist/linux-unpacked/hyper');
+  //     break;
 
-    case 'darwin':
-      pathToBinary = path.join(__dirname, '../dist/mac/Hyper.app/Contents/MacOS/Hyper');
-      break;
+  //   case 'darwin':
+  //     pathToBinary = path.join(__dirname, '../dist/mac/Hyper.app/Contents/MacOS/Hyper');
+  //     break;
 
-    case 'win32':
-      pathToBinary = path.join(__dirname, '../dist/win-unpacked/Hyper.exe');
-      break;
+  //   case 'win32':
+  //     pathToBinary = path.join(__dirname, '../dist/win-unpacked/Hyper.exe');
+  //     break;
 
-    default:
-      throw new Error('Path to the built binary needs to be defined for this platform in test/index.js');
-  }
+  //   default:
+  //     throw new Error('Path to the built binary needs to be defined for this platform in test/index.js');
+  // }
+
+  let pathToBinary = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
+  if (process.platform === 'win32') pathToBinary += '.cmd';
 
   app = new Application({
-    path: pathToBinary
+    path: pathToBinary,
+    args: ['--no-sandbox', path.join(__dirname, '../target')]
+    // chromeDriverArgs: ['--no-sandbox']
   });
 
   await app.start();
